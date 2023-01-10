ea5e849b1950118f2f8c1fb32a8c7ad04a5c1491
Mike Goren <mike@mortalwombat.net>
2023-01-10 11:40:07 -0600

    port site to front-end only

---
+++ README.md
@@ -0,0 +1,7 @@
+# Contra-Payments
+
+## Simple front-end site to sell contra dance tickets via PayPal
+
+## To Do
+- Webhook to Zapier to log transactions in Google spreadsheet
+- Webhook to Zapier to send email receipt
---
+++ index.js
@@ -0,0 +1,80 @@
+var sliding_scale_min = 15;
+var sliding_scale_max = 30;
+
+document.addEventListener('DOMContentLoaded', function() {
+  window.pricePerField = document.getElementById('admission-cost');
+  window.quantityField = document.getElementById('admission-quantity');
+  window.noteField = document.getElementById('note');
+  window.totalField = document.getElementById('total');
+  window.totalField2 = document.getElementById('total-2');
+  window.totalDetailsField = document.getElementById('total-details');
+  window.totalDetailsField2 = document.getElementById('total-details-2');
+  window.checkoutButton = document.getElementById('checkout');
+  window.backButton = document.getElementById('back');
+  window.paymentForm = document.getElementById('payment-form');
+  window.paymentSection = document.getElementById('payment');
+  updateTotal();
+  pricePerField.addEventListener('change', costUpdated, false);
+  quantityField.addEventListener('change', quantityUpdated, false);
+  quantityField.addEventListener('keyup', toggleNote, false);
+  checkoutButton.addEventListener('click', checkout, false);
+  backButton.addEventListener('click', back, false);
+}, false);
+
+var toggleNote = function() {
+  var quantity = quantityField.value;
+  if (!isNaN(quantity) && quantity > 1) {
+    noteField.style.display = 'block';
+  } else {
+    noteField.style.display = 'none';
+  }
+};
+
+var costUpdated = function() {
+  pricePerField.value = pricePerField.value.split('.')[0];
+  var pricePer = parseInt(pricePerField.value);
+  if (isNaN(pricePer) || pricePer > sliding_scale_max) {
+    pricePerField.value = sliding_scale_max;
+  } else if (pricePer < sliding_scale_min) {
+    pricePerField.value = sliding_scale_min;
+  }
+  updateTotal();
+};
+
+var quantityUpdated = function() {
+  quantityField.value = quantityField.value.split('.')[0];
+  var quantity = parseInt(quantityField.value);
+  if (isNaN(quantity) || quantity < 1) {
+    quantityField.value = '1';
+  } else if (quantity > 9) {
+    quantityField.value = '9';
+  }
+  toggleNote();
+  updateTotal();
+};
+
+var updateTotal = function() {
+  var pricePer = parseInt(pricePerField.value);
+  var quantity = parseInt(quantityField.value);
+  var total = pricePer * quantity;
+  totalField.textContent = total;
+  totalField2.textContent = total;
+  if (quantity > 1) {
+    totalDetailsField.textContent = '(' + quantity + ' admissions at $' + pricePer + ' each)';
+    totalDetailsField2.textContent = '(' + quantity + ' admissions at $' + pricePer + ' each)';
+  } else {
+    totalDetailsField.textContent = '(1 admission)';
+    totalDetailsField2.textContent = '(1 admission)';
+  }
+};
+
+var checkout = function(e) {
+  e.preventDefault();
+  paymentForm.classList.add('d-none');
+  paymentSection.classList.remove('d-none');
+};
+
+var back = function() {
+  paymentSection.classList.add('d-none');
+  paymentForm.classList.remove('d-none');
+};
